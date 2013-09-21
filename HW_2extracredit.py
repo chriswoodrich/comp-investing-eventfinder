cbcecdff87ad03422c36dce35ad63306a784566a
Chris <cwoodrich@gmail.com>
2013-09-21 01:08:04 -0700

    this runs the exra credit: 60 simulations

---
+++ HW_2extracredit.py
@@ -0,0 +1,114 @@
+'''
+(c) 2011, 2012 Georgia Tech Research Corporation
+This source code is released under the New BSD license.  Please see
+http://wiki.quantsoftware.org/index.php?title=QSTK_License
+for license details.
+
+The QuantSoftware ToolKit (QSTK) is released under the New BSD License as follows:
+
+Copyright (c) 2011, 2012, Georgia Tech Research Corporation (GTRC) All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+
+    Neither the name of the Georgia Tech Research Corporation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE GEORGIA TECH RESEARCH CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+
+Created on January, 23, 2013
+
+@author: Sourabh Bajaj
+@contact: sourabhbajaj@gatech.edu
+@summary: Event Profiler Tutorial
+'''
+
+
+import pandas as pd
+import numpy as np
+import math
+import copy
+import QSTK.qstkutil.qsdateutil as du
+import datetime as dt
+import QSTK.qstkutil.DataAccess as da
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkstudy.EventProfiler as ep
+
+"""
+Accepts a list of symbols along with start and end date
+Returns the Event Matrix which is a pandas Datamatrix
+Event matrix has the following structure :
+    |IBM |GOOG|XOM |MSFT| GS | JP |
+(d1)|nan |nan | 1  |nan |nan | 1  |
+(d2)|nan | 1  |nan |nan |nan |nan |
+(d3)| 1  |nan | 1  |nan | 1  |nan |
+(d4)|nan |  1 |nan | 1  |nan |nan |
+...................................
+...................................
+Also, d1 = start date
+nan = no information about any event.
+1 = status bit(positively confirms the event occurence)
+"""
+
+
+
+def find_events(ls_symbols, d_data, price_point):
+    ''' Finding the event dataframe '''
+    df_actual_close = d_data['actual_close']
+    ts_market = df_actual_close['SPY']
+
+    print "Finding Events"
+
+    # Creating an empty dataframe
+    df_events = copy.deepcopy(df_actual_close)
+    df_events = df_events * np.NAN
+
+    # Time stamps for the event range
+    ldt_timestamps = df_actual_close.index
+
+    for s_sym in ls_symbols:
+        for i in range(1, len(ldt_timestamps)):
+            # Calculating the returns for this timestamp
+            f_symprice_today = df_actual_close[s_sym].ix[ldt_timestamps[i]]
+            f_symprice_yest = df_actual_close[s_sym].ix[ldt_timestamps[i - 1]]
+            #f_marketprice_today = ts_market.ix[ldt_timestamps[i]]
+            #f_marketprice_yest = ts_market.ix[ldt_timestamps[i - 1]]
+            #f_symreturn_today = (f_symprice_today / f_symprice_yest) - 1
+            #f_marketreturn_today = (f_marketprice_today / f_marketprice_yest) - 1
+
+            # if the price was >= 5 yesterday and < 5 today  TRIGGER EVENT
+            if f_symprice_today < price_point and f_symprice_yest >= price_point:
+                df_events[s_sym].ix[ldt_timestamps[i]] = 1
+
+    return df_events
+
+
+if __name__ == '__main__':
+    dt_start = dt.datetime(2008, 1, 1)
+    dt_end = dt.datetime(2009, 12, 31)
+    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
+
+    dataobj = da.DataAccess('Yahoo')
+    #ls_symbols = dataobj.get_symbols_from_list('sp5002008') for 2008
+    ls_symbols = dataobj.get_symbols_from_list('sp5002012') #for 2012
+    ls_symbols.append('SPY')
+
+    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+    ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+    d_data = dict(zip(ls_keys, ldf_data))
+
+    for s_key in ls_keys:
+        d_data[s_key] = d_data[s_key].fillna(method='ffill')
+        d_data[s_key] = d_data[s_key].fillna(method='bfill')
+        d_data[s_key] = d_data[s_key].fillna(1.0)
+
+    for dollar in range(1,60):  #gets all data from the S&P based on 1-60 thresholds
+        df_events = find_events(ls_symbols, d_data, dollar)
+        dollar_name = str(dollar)
+        print "Creating Study" , dollar_name
+        #graph_name = raw_input("name your graph, please: ")
+        ep.eventprofiler(df_events, d_data, i_lookback=20, i_lookforward=20,
+                s_filename=dollar_name, b_market_neutral=True, b_errorbars=True,
+                s_market_sym='SPY')
